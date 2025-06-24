72e60f4bf8ba92580b754da5ebabefb0934d1a63
rohan-gogate <rohgogate@gmail.com>
2025-06-24 18:48:20 -0400

    preprocessing done

---
+++ preprocess.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+
+def preprocess_data(df):
+    X = df.drop(columns=["binomial_price", "mc_price"])
+    y = df["mc_price", "binomial_price"]
+
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.36, random_state=0)
+    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=20/36, random_state=0)
+
+    x_scaler = MinMaxScaler().fit(X_train)
+    y_scaler = MinMaxScaler().fit(y_train)
+
+    X_train_scaled = x_scaler.transform(X_train)
+    X_val_scaled = x_scaler.transform(X_val)
+    X_test_scaled = x_scaler.transform(X_test)
+    
+    y_train_scaled = y_scaler.transform(y_train)
+    y_val_scaled = y_scaler.transform(y_val)
+    y_test_scaled = y_scaler.transform(y_test)
+
+    return {
+        'X_train': X_train_scaled, 'y_train': y_train_scaled,
+        'X_val': X_val_scaled, 'y_val': y_val_scaled,
+        'X_test': X_test_scaled, 'y_test': y_test_scaled,
+        'x_scaler': x_scaler, 'y_scaler': y_scaler}
